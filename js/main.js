fabfd332f7a57cf72b1fc7dda06533b85a0434de
NovakVladimir <vladimir.novak.89@i.ua>
2020-11-13 23:12:54 +0200

    Add html and css. Begin to create JS

---
+++ js/main.js
@@ -0,0 +1,11 @@
+function getInput() {
+    let numb = document.getElementById("money").value;
+    let curr; 
+    switch(true) {
+        case document.getElementById("currency_1").checked: curr=""
+    }
+}
+
+function changeCurrency() {
+
+}
