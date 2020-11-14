29a61bef606de0a9c0f558324f34deedc47b0edd
NovakVladimir <vladimir.novak.89@i.ua>
2020-11-14 12:21:26 +0200

    Add JS and finish project

--- js/main.js
+++ js/main.js
@@ -1,11 +1,28 @@
-function getInput() {
-    let numb = document.getElementById("money").value;
-    let curr; 
-    switch(true) {
-        case document.getElementById("currency_1").checked: curr=""
-    }
-}
-
 function changeCurrency() {
+    let numb = document.getElementById("money").value;
+    let rateEur = 1.18;
+    let rateUan = 0.15;
+    let rateAzn = 0.61;
+    var result;
+    let curr;
 
+    switch(true) {
+        case document.getElementById("currency_1").checked===true: 
+            curr = "EUR";
+            result = numb / rateEur;
+        break;
+        case document.getElementById("currency_2").checked===true:
+            curr = "UAN";
+            result = numb / rateUan;
+        break;
+        case document.getElementById("currency_3").checked===true: 
+            curr = "AZN";
+            result = numb / rateAzn;
+        break;
+    }
+
+    result = `You'll get ${result.toFixed(2)} ${curr}`;
+    document.getElementById("message").innerHTML = result;
+    document.getElementById("message").style.backgroundColor = "rgba(20,200,200,0.8)";
+    return result;
 }
